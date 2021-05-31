421c60f22bd254035e10b1dc4efa460b20a594d1
Augustin <augustin.berne.pro@gmail.com>
2021-05-31 13:53:06 +0200

    Fonctionnalité 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fonctionnalité 1
+// ------------------------------     Fonctionnalité 1     ------------------------------
 
 // let footerclick = document.getElementsByTagName('footer')[0];
 
@@ -8,12 +8,16 @@
 //     footerclick = console.log("clique");
 // }
 
-// Fonctionnalité 1-bis
+// ------------------------------     Fonctionnalité 1-bis     ------------------------------
 
 let footerclick = document.getElementsByTagName('footer')[0];
 
 footerclick.addEventListener('click',footerdisplay);
+let count = 0
 
 function footerdisplay(){
-    footerclick = console.log("clique");
+    count ++
+    footerclick = console.log("Clic numéro " + count);
 }
+
+// ------------------------------     Fonctionnalité 2     ------------------------------
